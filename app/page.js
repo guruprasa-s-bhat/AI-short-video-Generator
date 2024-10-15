69965ebf6d5831259465a1e1c2890d5260c46b3a
Guruprasad Bhat <guruprasadbhat106@gmail.com>
2024-10-15 19:22:39 +0530

    Update color and font

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Button>heyy</Button>
+      <h1>hello user</h1>
+      <Button>subsribe</Button>
+      <UserButton />
     </div>
   );
 }
--- app/provider.js
+++ app/provider.js
@@ -1,6 +1,41 @@
-import React from "react";
+"use client";
+import { db } from "@/configs/db";
+import { Users } from "@/configs/schema";
+import { useUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import React, { useEffect } from "react";
 
 const Provider = ({ children }) => {
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      isNewUser();
+    }
+  }, [user]);
+
+  const isNewUser = async () => {
+    try {
+      const result = await db
+        .select()
+        .from(Users)
+        .where(eq(Users.email, user?.primaryEmailAddress?.emailAddress));
+
+      console.log(result);
+
+      if (result.length === 0) {
+        await db.insert(Users).values({
+          name: user.fullName,
+          email: user?.primaryEmailAddress?.emailAddress,
+          imageUrl: user?.imageUrl || null, // Default to null if not available
+          subscription: false, // Set subscription to false by default
+        });
+      }
+    } catch (error) {
+      console.error("Database operation failed:", error);
+    }
+  };
+
   return <div>{children}</div>;
 };
 
